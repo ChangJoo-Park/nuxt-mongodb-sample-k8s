4e4d727edc7f093083909e328301c6884c613489
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-09 17:56:54 +0900

    remove useless console

--- plugins/repository.js
+++ plugins/repository.js
@@ -2,15 +2,13 @@ import createRepository from '~/api/repository'
 
 export default (ctx, inject) => {
   const repositoryWithAxios = createRepository(ctx.$axios)
-  console.log('repositoryWithAxios => ', repositoryWithAxios)
+
   const repositories = {
     create: repositoryWithAxios.create,
     read: repositoryWithAxios.read,
     update: repositoryWithAxios.update,
     delete: repositoryWithAxios.delete
   }
-
-  console.log('repositories => ', repositories)
 
   inject('repo', repositories)
 }
