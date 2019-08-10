c3851af7da56c8600ab0b75d0f50fe8aa1c103ef
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-10 10:12:08 +0900

    implement listing index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-    <h1>Listing</h1>
-    <nuxt-child />
+    <main>
+      <ul>
+        <nuxt-link
+          v-for="item in list"
+          :key="item._id"
+          :to="`/${item._id}`"
+          tag="li"
+        >
+          {{ item._id }}
+        </nuxt-link>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-
+  asyncData ({ app, error }) {
+    return app.$repo.read()
+      .then(({ data }) => {
+        return {
+          list: data
+        }
+      }).catch((err) => {
+        error(err.response)
+      })
+  }
 }
 </script>
 
