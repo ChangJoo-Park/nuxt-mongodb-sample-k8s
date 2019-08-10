ede7227d55786331e6df17512eb8b4c4a37b86a4
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-10 10:31:18 +0900

    Implement show

--- pages/index/_id.vue
+++ pages/index/_id.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>View</h1>
+    <div>
+      {{ item }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  asyncData ({ app, params, error }) {
+    return app.$repo.read(params.id)
+      .then(({ data }) => {
+        return {
+          item: data
+        }
+      }).catch((err) => {
+        error(err.response)
+      })
+  }
 }
 </script>
 
