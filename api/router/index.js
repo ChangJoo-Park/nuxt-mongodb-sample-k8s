7762914497f40b347c5ea30c9d7b20e47fa689b0
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-10 10:31:08 +0900

    Add read api for update

--- api/router/index.js
+++ api/router/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { mongo } from './mongodb'
+import { ObjectId } from 'mongodb'
 
 export const createRouter = () => {
   const router = express.Router()
@@ -8,24 +9,25 @@ export const createRouter = () => {
 
   router.get('/api/employees', (req, res) => {
     mongo()
-      .then(db => {
-        const collection = db.collection('employee')
-        collection.find().toArray()
-          .then(result => {
-            res.json([])
-          })
-      })
-      .catch(err => {
-        res.json(err)
-    })
+      .then(db => db.collection('employee').find().toArray())
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
   })
 
   router.post('/api/employees', (req, res) => {
-    return res.json([])
+    const { name = '' } = req.body
+    mongo()
+      .then(db => db.collection('employee').insertOne({ name }))
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
   })
 
-  router.get('/api/employees/:id', (req, res) => {
-    return res.json({})
+  router.get('/api/employees/:_id', (req, res) => {
+    const { _id } = req.params
+    mongo()
+      .then(db => db.collection('employee').findOne({ _id: ObjectId(_id) }))
+      .then(result => res.json(result))
+      .catch(err => res.status(400).json(err))
   })
 
   router.put('/api/employees/:id', (req, res) => {
