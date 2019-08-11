14cc0f56ae0a3b1c0c07f470972076564c359a3c
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-11 16:30:02 +0900

    Update link style

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div class="w-full h-full">
     <div class="container">
       <nav class="mb-4">
-        <nuxt-link to="/" class="button--green">
+        <nuxt-link to="/" class="text-lg no-underline hover:underline hover:text-blue-500 mr-4">
           Home
         </nuxt-link>
-        <nuxt-link to="/new" class="button--green">
+        <nuxt-link to="/new" class="text-lg no-underline hover:underline hover:text-blue-500">
           New Employee
         </nuxt-link>
       </nav>
