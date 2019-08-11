c3fededa9b9ba0175ab6798dca4646e0f92a2cca
ChangJoo Park <changjoo.park@iplateia.com>
2019-08-11 16:30:45 +0900

    Update listing style

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <main>
+      <h1 class="page-title">
+        Employee Listing
+      </h1>
       <ul>
         <nuxt-link
           v-for="item in list"
           :key="item._id"
           :to="`/${item._id}`"
           tag="li"
+          class="border px-2 py-4 mb-2 border-l-4 cursor-pointer hover:bg-blue-400 hover:text-white hover:border-blue-500 rounded-sm"
         >
-          {{ item.name }}
+          <div>
+            {{ item._id }}
+          </div>
+          <div class="font-bold">
+            {{ item.name }}
+          </div>
         </nuxt-link>
       </ul>
     </main>
